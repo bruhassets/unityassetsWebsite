c662049c1157204f26346752e5998a4e6e5b0d37
Kostyantyn Grygoryev <grysha39@icloud.com>
2020-07-05 11:55:01 +0200

    fixes

--- contactform.php
+++ contactform.php
@@ -3,7 +3,7 @@
 if (isset ($_POST['submit']))
 {
     $name = $_POST['name'];
-    $mailFrom = $_POST['email'];
+    $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
 
     $mailTo = "brushassets@outlook.com";
